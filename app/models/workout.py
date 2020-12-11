b13d7ad4f086e86a94c57cf39a3ade16f23378f0
Stanley Houcke <stanhoucke@gmail.com>
2020-12-11 18:22:23 +0000

    passed class tests for workout

--- app/models/workout.py
+++ app/models/workout.py
@@ -1,11 +1,11 @@
 class Workout():
-    def __init__(self, name, category, upcoming, date, time, id = None):
+    def __init__(self, name, category, upcoming, date, start_time, id = None):
         self.name = name
         self.category = category
         self.upcoming = upcoming
         self.date = date
-        self.time = time
+        self.start_time = start_time
         self.id = id
 
-    def date_time(self, date, time):
-        return f"{date} {time}"
+    def date_time(self):
+        return f"{self.date} {self.start_time}"
--- app/tests/workout_test.py
+++ app/tests/workout_test.py
@@ -3,7 +3,26 @@ from models.workout import Workout
 
 class TestWorkout(unittest.TestCase):
     def setUp(self):
-        pass
+        self.workout_1 = Workout("Just Skate", "freestyle", True, "12/12/2020", "08:00")
+
+    def test_workout_has_name(self):
+        self.assertEqual("Just Skate", self.workout_1.name)
+
+    def test_workout_has_category(self):
+        self.assertEqual("freestyle", self.workout_1.category)
+
+    def test_workout_has_upcoming(self):
+        self.assertEqual(True, self.workout_1.upcoming)
+
+    # Date and time tests
+    def test_workout_has_date(self):
+        self.assertEqual("12/12/2020", self.workout_1.date)
+
+    def test_workout_has_start_time(self):
+        self.assertEqual("08:00", self.workout_1.start_time)
+
+    def test_date_time__returns_date_and_time(self):
+        self.assertEqual("12/12/2020 08:00", self.workout_1.date_time())
     
 
     
