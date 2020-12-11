d02cad440d21cb6204b30465585e68eb96e15dd8
Stanley Houcke <stanhoucke@gmail.com>
2020-12-11 19:00:16 +0000

    created db tables

---
+++ app/db/gym_app.sql
@@ -0,0 +1,26 @@
+DROP TABLE bookings;
+DROP TABLE members;
+DROP TABLE workouts;
+
+CREATE TABLE workouts (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    category VARCHAR(255),
+    upcoming BOOLEAN,
+    date VARCHAR(255),
+    start_time VARCHAR(255)
+);
+
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    gender VARCHAR(255),
+    age INT
+);
+
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    member_id INT REFERENCES members(id) ON DELETE CASCADE,
+    workout_id INT REFERENCES workouts(id) ON DELETE CASCADE
+);
