85f542ca8df5f39adef9dff59c3b4c911484ebb1
Stanley Houcke <stanhoucke@gmail.com>
2020-12-12 19:23:15 +0000

    added create new class route

--- app/controllers/workouts_controller.py
+++ app/controllers/workouts_controller.py
@@ -15,3 +15,23 @@ def show_workout(id):
     members = workout_repository.members(workout)
     return render_template("workouts/show.html", workout=workout, members=members)
 
+@workouts_blueprint.route("/workouts/new")
+def new_workout():
+    return render_template("workouts/new.html")
+
+@workouts_blueprint.route('/workouts', methods=['POST'])
+def create_workout():
+    # Form data
+    name        = request.form['name']
+    category    = request.form['category']
+    upcoming    = request.form['upcoming']
+    date        = request.form['date']
+    start_time  = request.form['start_time']
+    # Create new Workout object
+    workout = Workout(name, category, upcoming, date, start_time)
+    # Save to db
+    workout_repository.save(workout)
+    # Redirect
+    return redirect('/workouts')
+
+
