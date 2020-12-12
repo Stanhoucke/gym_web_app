717fd439f04adf4658c7b5684cd0ebe4c82e9692
Stanley Houcke <stanhoucke@gmail.com>
2020-12-12 10:28:07 +0000

    added save function to workout repository

--- app/console.py
+++ app/console.py
@@ -1,10 +1,17 @@
 from models.member import Member
+from models.workout import Workout
 import repositories.member_repository as member_repository
+import repositories.workout_repository as workout_repository
 
 # member_repository.delete_all()
 
 member_1 = Member("Dario", "Cologna", "male", 34)
 member_repository.save(member_1)
+workout_1 = Workout("Just Skate", "freestyle", True, "12/12/2020", "08:00")
+workout_repository.save(workout_1)
+workout_2 = Workout("Double Poling", "classic", False, "10/12/2020", "14:00")
+workout_repository.save(workout_2)
+
 
 member_1.age = 35
 member_repository.update(member_1)
---
+++ app/repositories/workout_repository.py
@@ -0,0 +1,66 @@
+from db.run_sql import run_sql
+from models.workout import Workout
+from models.member import Member
+
+# Create
+def save(workout):
+    sql = "INSERT INTO workouts (name, category, upcoming, date, start_time) VALUES (%s, %s, %s, %s, %s) RETURNING id"
+    values = [workout.name, workout.category, workout.upcoming, workout.date, workout.start_time]
+    result = run_sql(sql, values)
+    workout.id = result[0]['id']
+    return workout
+
+# Read
+def select(id):
+    workout = None
+
+    sql = "SELECT * FROM workouts WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        workout = Workout(result['name'], result['category'], result['upcoming'], result['date'], result['start_time'], result['id'])
+    return workout
+
+def select_all():
+    workouts = []
+
+    sql = "SELECT * FROM workouts"
+    results = run_sql(sql)
+
+    for row in results:
+        workout = Workout(row['name'], row['category'], row['upcoming'], row['date'], row['start_time'], row['id'])
+        workouts.append(workout)
+    return workouts
+
+def select_upcoming():
+    workouts = []
+
+    sql = "SELECT * FROM workouts"
+    results = run_sql(sql)
+
+    for row in results:
+        workout = Workout(row['name'], row['category'], row['upcoming'], row['date'], row['start_time'], row['id'])
+        if workout.upcoming:
+            workouts.append(workout)
+    return workouts
+
+
+def members(workout):
+    pass
+
+# Update
+def update(workout):
+    sql = "UPDATE workouts SET (name, cateogry, upcoming, date, start_time) = (%s, %s, %s, %s, %s) WHERE id = %s"
+    values = [workout.name, workout.category, workout.upcoming, workout.date, workout.start_time, workout.id]
+    run_sql(sql, values)
+
+# Delete
+def delete(id):
+    sql = "DELETE FROM workouts WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+def delete_all():
+    sql = "DELETE FROM workouts"
+    run_sql(sql)
