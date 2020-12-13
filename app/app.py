0ba7f515a04d44aea26a550321264b0ce584314c
Stanley Houcke <stanhoucke@gmail.com>
2020-12-13 19:28:31 +0000

    updated dashboard template

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from controllers.workouts_controller import workouts_blueprint
 from controllers.bookings_controller import bookings_blueprint
 
 import repositories.workout_repository as workout_repository
+import repositories.booking_repository as booking_repository
 
 app = Flask(__name__)
 
@@ -15,7 +16,8 @@ app.register_blueprint(bookings_blueprint)
 @app.route('/')
 def home():
     workouts = workout_repository.select_upcoming()
-    return render_template('index.html', workouts=workouts)
+    bookings = booking_repository.select_all()
+    return render_template('index.html', workouts=workouts, bookings=bookings)
 
 if __name__ == '__main__':
     app.run(debug=True)
